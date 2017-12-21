510f2672841f79f6d5a12ba2909252fd6db79966
Jarry <JarryDr@outlook.com>
2017-12-21 15:26:57 +0800

    2017年12月21日 15:26:54

---
+++ MyBatisDemo01/src/com/learn/chapter2/main/Chapter2Main.java
@@ -0,0 +1,34 @@
+package com.learn.chapter2.main;
+
+import java.io.IOException;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.learn.chapter2.mapper.RoleMapper;
+import com.learn.chapter2.po.Role;
+import com.learn.chapter2.util.SqlSessionFactoryUtil;
+
+public class Chapter2Main {
+	
+	public static void main(String[] args) throws IOException{
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = SqlSessionFactoryUtil.openSqlSession();
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			Role role = new Role();
+			role.setRoleName("testName");
+			role.setNote("testNote");
+			roleMapper.insertRole(role);
+			roleMapper.deleteRole(1L);
+			sqlSession.commit();
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			sqlSession.rollback();
+		}finally {
+			if (sqlSession!=null) {
+				sqlSession.close();
+			}
+		}
+	}
+	
+}
